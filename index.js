4813324a9ec48409ec260429d8a6a049c9d90da6
Riley Burke <rjb489@nau.edu>
2024-09-11 21:18:44 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,81 @@
+// Necessary Imports
 import express from "express";
+import bodyParser from "body-parser";
+
+// Constants
 const app = express();
 const port = 3000;
 
-// 2 args: (1) is Port, (2) is Callback Function
-app.listen(port, () => {console.log(`Server running on port ${port}.`)})
+// Variables
+var blogPostArray = [];
+
+// EJS & BodyParser
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded( {extended: true} ));
+
+// BLOG POST BLOCK
+// ===================================================
+// Blog Post Handle
+app.get("/", (req, res) => { 
+   res.render("index.ejs", { blogPostArray }) 
+});
+
+// Updating the Blog Array
+app.post("/new-post", (req, res) => {
+
+   // Retrieve Data
+   var title = req.body.title;
+   var content = req.body.content;
+   var author = req.body.author;
+
+   // Create the Post
+   var newPost = { title, content, author, time: new Date() };
+
+   // Update and Redirect User
+   blogPostArray.push(newPost);
+   res.redirect("/");
+});
+// ===================================================
+
+// EDIT BLOCK
+// ===================================================
+
+// Edit Page Handle
+app.get("/edit/:id", (req, res) => {
+   // Get the posts position
+   var postID = req.params.id;
+   var blogPost = blogPostArray[postID];
+   
+   // Render The Post
+   res.render("edit", { blogPost: blogPost, postID: postID });
+});
+
+// Update the Edits
+app.post( "/edit/:id", (req, res) => {
+
+   var postID = req.params.id;
+
+   // Update Array with Edited Post
+   blogPostArray[postID] = {
+      title: req.body.title, content: req.body.content, 
+      author: req.body.author, time: blogPostArray[postID].time
+   }
+
+   res.redirect("/");
+});
+
+// ===================================================
+
+// Handle Deleting
+app.post("/delete/:id", (req, res) => {
+   var postID = req.params.id;
+
+   delete blogPostArray[postID];
+
+   res.redirect("/")
+});
+
+// Basic Listening for Console
+app.listen(port, () => {
+   console.log(`Server running on port ${port}.`)})
